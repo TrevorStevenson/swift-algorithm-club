cdff7ac38b70d3ca809a47ea528bf2aedd9431dd
Trevor Stevenson <trevor.stevenson@duke.edu>
2018-08-20 20:43:38 -0400

    Add NetworkNode class

---
+++ Network Flows/NetworkGraph.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkGraph.swift
+//  
+//
+//  Created by Trevor Stevenson on 8/20/18.
+//
+
+import Foundation
+
+class NetworkNode<T> {
+    var value: T?
+    var neighbors: [NetworkNode: Int] = []
+}
+
+class NetworkGraph {
+    
+}
