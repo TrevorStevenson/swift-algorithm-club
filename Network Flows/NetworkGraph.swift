f14b7f0c3a6cdabc78062667c7ceb349d295be9f
Trevor Stevenson <trevor.stevenson@duke.edu>
2018-08-22 18:13:25 -0400

    Layout remaining function implementations

--- Network Flows/NetworkGraph.swift
+++ Network Flows/NetworkGraph.swift
@@ -18,15 +18,28 @@ class NetworkNode<T> {
 }
 
 class NetworkPath<T> {
-    var start: NetworkNode?
+    var start: NetworkNode<T>?
     var next: NetworkPath<T>?
     
     var flow: Int = 0
+    
+    init(start: NetworkNode<T>?) {
+        self.start = start
+    }
 }
 
 class NetworkGraph<T> {
     var source: NetworkNode<T>?
     var target: NetworkNode<T>?
+    
+    var flow: [[Int]] = []
+    
+    init(source: NetworkNode<T>?, target: NetworkNode<T>?, nodes: Int) {
+        self.source = source
+        self.target = target
+        
+        flow = Array(count: nodes, repeatedValue: Array(count: nodes, repeatedValue: 0))
+    }
     
     // Recursive DFS implementation
     
@@ -57,5 +70,36 @@ class NetworkGraph<T> {
         }
 
         return nil
+    }
+    
+    // Find path from source to target and return that path
+    
+    func findPath() -> NetworkPath<T>? {
+        guard let start = source, let end = target else { return nil }
+        
+        var stack = [start]
+        var path = NetworkPath<T>(start: start)
+        let head = path
+        
+        while stack.count > 0 {
+            let top = stack.popLast()
+            path.next = NetworkPath<T>(start: top)
+            
+            if top == target { return head }
+        }
+        
+        return nil
+    }
+    
+    // Augment the graph with a flow
+    
+    func augmentPath(path: NetworkPath<T>, flow: Int) {
+        
+    }
+    
+    // Get the total current flow of the graph
+    
+    func getFlowValue() -> Int {
+        
     }
 }
